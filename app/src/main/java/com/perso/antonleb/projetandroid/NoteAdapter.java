39acef523d9dfc44e472bc67f187eb1e5a2b9cbf
Antonin LEBRARD <antonleb@cisco.com>
2015-11-27 17:53:27 +0100

    [AL] Preparing the connect Activity, and modify the buttons in the notes to ImageButtons with better image

--- app/src/main/java/com/perso/antonleb/projetandroid/NoteAdapter.java
+++ app/src/main/java/com/perso/antonleb/projetandroid/NoteAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -33,7 +34,7 @@ public class NoteAdapter extends ArrayAdapter<INote> {
         }
         final INote note = getItem(position);
         ((TextView)root.findViewById(R.id.note)).setText(note.getNote());
-        ((Button)root.findViewById(R.id.google)).setOnClickListener(new View.OnClickListener() {
+        ((ImageButton)root.findViewById(R.id.google)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String url = urlPrefix + note.getNote();
@@ -42,7 +43,7 @@ public class NoteAdapter extends ArrayAdapter<INote> {
                 getContext().startActivity(i);
             }
         });
-        ((Button)root.findViewById(R.id.delete)).setOnClickListener(new View.OnClickListener() {
+        ((ImageButton)root.findViewById(R.id.delete)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 inWithin.deleteNote(position);
